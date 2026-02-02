5360994219ac70385886dbf8cfaf4b8dd229725d
John <john@Johns-MacBook-Air.local>
2026-02-01 22:55:32 -0500

    feat(analytics): add Google Tag Manager integration- Add GTM script in head section (GTM-W7V4TD6P)- Add GTM noscript iframe after body opening tag- Enable tracking and analytics for the platform

--- app/layout.tsx
+++ app/layout.tsx
@@ -140,6 +140,15 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W7V4TD6P');`
+          }}
+        />
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" type="image/svg+xml" href="/icon.svg" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
@@ -304,6 +313,14 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W7V4TD6P"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Providers>
           <PWARegister />
           <TestModeBanner />
